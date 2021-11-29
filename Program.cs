816198da032f9d58cfac89b3ca7d677e8a5f05e9
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-29 03:11:13 -0300

    Initialize project.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Linq;
+using static System.Console;
+
+LinqWithArrayOfStrings();
+
+void LinqWithArrayOfStrings()
+{
+    var names =  new string[]
+    {
+        "Michael", "Pam", "Jim", "Dwight", "Angela",
+        "Kevin", "Toby", "Creed"
+    };
+    var query = names.Where(
+        new Func<string, bool>(NameLongerThanFour));
+    foreach (string item in query) WriteLine(item);
+}
+
+bool NameLongerThanFour(string name) => name.Length > 4;
